71cc0ff90aae9b1e0e3c298b3055e06d2360bac6
Tarcisio Maciel <tarcisio.maciel@gmgroupbrasil.com.br>
2024-07-27 14:11:49 -0300

    alteração do video de apresentacao e inclusão do carrousel.

--- site-web/js/main.js
+++ site-web/js/main.js
@@ -5,3 +5,149 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove("menu-expanded");
 }
+
+class FormSubmit {
+  constructor(settings) {
+    this.settings = settings;
+    this.form = document.querySelector(settings.form);
+    this.formButton = document.querySelector(settings.button);
+
+    if (this.form) {
+      this.url = this.form.getAttribute("action");
+    }
+
+    this.sendForm = this.sendForm.bind(this);
+  }
+
+  displaySuccess() {
+    this.form.innerHTML = this.settings.success;
+  }
+
+  displayError() {
+    this.form.innerHTML = this.settings.error;
+  }
+
+  getFormObject() {
+    const formObject = {};
+    const fields = this.form.querySelectorAll("[name]");
+
+    fields.forEach((field) => {
+      formObject[field.getAttribute("name")] = field.value;
+    });
+
+    return formObject;
+  }
+
+  onSubmission(event) {
+    event.preventDefault();
+    this.formButton.disabled = true;
+    this.formButton.innerHTML = "Enviando ...";
+  }
+
+  async sendForm(event) {
+    try {
+      this.onSubmission(event);
+
+      const response = await fetch(this.url, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          accept: "application/json",
+        },
+        body: JSON.stringify(this.getFormObject()),
+      });
+
+      if (response.ok) {
+        this.displaySuccess();
+      } else {
+        this.displayError();
+      }
+    } catch (error) {
+      this.displayError();
+      console.error(error);
+    } finally {
+      this.formButton.disabled = false;
+      this.formButton.innerHTML = "Enviar";
+    }
+  }
+
+  init() {
+    if (this.form) this.formButton.addEventListener("click", this.sendForm);
+  }
+}
+
+const formSubmit = new FormSubmit({
+  form: "[data-form]",
+  button: "[data-button]",
+  success: "<h3 class='success'>Mensagem enviada com sucesso.</h3>",
+  error: "<h3 class='error'>Não foi possível enviar sua mensagem.</h3>",
+});
+
+formSubmit.init();
+
+class Carousel {
+  constructor(element) {
+    this.carousel = element;
+    this.slidesContainer = this.carousel.querySelector(
+      "[data-carousel-slides-container]"
+    );
+    this.slides = this.slidesContainer.children;
+    this.prevButton = this.carousel.querySelector(
+      "[data-carousel-button-previous]"
+    );
+    this.nextButton = this.carousel.querySelector(
+      "[data-carousel-button-next]"
+    );
+    this.totalSlides = this.slides.length;
+    this.currentIndex = 0;
+    this.visibleSlides = window.innerWidth <= 767 ? 1 : 5;
+    this.autoplayInterval = 3000; // Intervalo do autoplay em milissegundos
+
+    this.prevButton.addEventListener("click", () => this.prevSlide());
+    this.nextButton.addEventListener("click", () => this.nextSlide());
+
+    window.addEventListener("resize", () => {
+      this.visibleSlides = window.innerWidth <= 767 ? 1 : 5;
+      this.updateSlidesPosition();
+    });
+
+    this.startAutoplay();
+  }
+
+  updateSlidesPosition() {
+    const offset = -(this.currentIndex * (100 / this.visibleSlides));
+    this.slidesContainer.style.transform = `translateX(${offset}%)`;
+  }
+
+  prevSlide() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.totalSlides - this.visibleSlides;
+    }
+    this.updateSlidesPosition();
+  }
+
+  nextSlide() {
+    if (this.currentIndex < this.totalSlides - this.visibleSlides) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+    this.updateSlidesPosition();
+  }
+
+  startAutoplay() {
+    this.autoplay = setInterval(() => this.nextSlide(), this.autoplayInterval);
+  }
+
+  stopAutoplay() {
+    if (this.autoplay) {
+      clearInterval(this.autoplay);
+    }
+  }
+}
+
+document
+  .querySelectorAll("[data-carousel]")
+  .forEach((carousel) => new Carousel(carousel));
--- site-web/js/pixi.mask.js
+++ site-web/js/pixi.mask.js
@@ -1,16 +1,19 @@
-$(document).ready(function(){
-	
-	var SPMaskBehavior = function (val) {
-		return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
-	},
-	spOptions = {
-		onKeyPress: function(val, e, field, options) {
-			field.mask(SPMaskBehavior.apply({}, arguments), options);
-		}
-	};
+$(document).ready(function () {
+  var SPMaskBehavior = function (val) {
+      return val.replace(/\D/g, "").length === 11
+        ? "(00) 00000-0000"
+        : "(00) 0000-00009";
+    },
+    spOptions = {
+      onKeyPress: function (val, e, field, options) {
+        field.mask(SPMaskBehavior.apply({}, arguments), options);
+      },
+    };
 
-	$('.telefone').mask(SPMaskBehavior, spOptions);
-})
+  $(".telefone").mask(SPMaskBehavior, spOptions);
+
+  $(".celular").mask("(00) 00000-0000");
+});
 
 /**
 *** Exemplos de mascaras
